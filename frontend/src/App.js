42c30521a9e6d3257c3bbf1285ef019565ae937d
Michel <patifeg@gmail.com>
2022-04-21 11:40:13 -0300

    Adicionando detalhes do produto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,33 @@
-import React from 'react'
-import data from './data';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">Emotion &amp; Kiddos</a>
-        </div>
-        <div>
-          <a href="/cart">Carrinho</a>
-          <a href="/signin">Entrar</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {
-            data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                </div>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">
-                  R${product.price}
-                </div>
-              </div>
-            )
-            )
-          }
-
-        </div>
-      </main>
-      <footer className="row center">
-        Todos direitos reservados
-      </footer>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <a className="brand" href="/">Emotion &amp; Kiddos</a>
+                    </div>
+                    <div>
+                        <a href="/cart">Carrinho</a>
+                        <a href="/signin">Entrar</a>
+                    </div>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/product/:id" element={<ProductScreen />}></Route>
+                        <Route path="/" element={<HomeScreen />} exact></Route>
+                    </Routes>
+                </main>
+                <footer className="row center">
+                    Todos direitos reservados
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
