0d7606dcd0b7d783f0543831013aff45315edd10
Michel <patifeg@gmail.com>
2022-04-22 22:24:27 -0300

    Incluindo Redux para tela de detalhes do produto

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,69 +1,86 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from "react-router-dom";
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
 
 export default function ProductScreen(props) {
+    const dispatch = useDispatch();
     const { id } = useParams();
-    const product = data.products.find((x) => x._id === id);
-    if (!product) {
-        return <div>Produto não encontrado!</div>;
-    }
+    const productId = id;
+    const productDetails = useSelector(state => state.productDetails);
+    const { loading, error, product } = productDetails;
+
+    useEffect(() => {
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
+
     return (
         <div>
-            <Link to="/">Voltar</Link>
-            <div className="row top">
-                <div className="col-2">
-                    <img className='large' src={product.image} alt={product.name}></img>
-                </div>
-                <div className="col-1">
-                    <ul>
-                        <li>
-                            <h1>{product.name}</h1>
-                        </li>
-                        <li>
-                            <Rating
-                                rating={product.rating}
-                                numReviews={product.numReviews}
-                            ></Rating>
-                        </li>
-                        <li>
-                            Preço : R${product.price}
-                        </li>
-                        <li>
-                            Descrição:
-                            <p>{product.description}</p>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-1">
-                    <div className="card card-body">
-                        <ul>
-                            <li>
-                                <div className="row">
-                                    <div>Preço</div>
-                                    <div className="price">R${product.price}</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="row">
-                                    <div>Disponibilidade</div>
-                                    <div>
-                                        {product.countInStock > 0 ? (
-                                            <span className="success">Em Estoque</span>
-                                        ) : (
-                                            <span className="danger">Indisponível</span>
-                                        )}
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <button className="primary block">Adicionar ao Carrinho</button>
-                            </li>
-                        </ul>
+            {loading ? (
+                <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+                <div>
+                    <Link to="/">Voltar</Link>
+                    <div className="row top">
+                        <div className="col-2">
+                            <img className='large' src={product.image} alt={product.name}></img>
+                        </div>
+                        <div className="col-1">
+                            <ul>
+                                <li>
+                                    <h1>{product.name}</h1>
+                                </li>
+                                <li>
+                                    <Rating
+                                        rating={product.rating}
+                                        numReviews={product.numReviews}
+                                    ></Rating>
+                                </li>
+                                <li>
+                                    Preço : R${product.price}
+                                </li>
+                                <li>
+                                    Descrição:
+                                    <p>{product.description}</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-1">
+                            <div className="card card-body">
+                                <ul>
+                                    <li>
+                                        <div className="row">
+                                            <div>Preço</div>
+                                            <div className="price">R${product.price}</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="row">
+                                            <div>Disponibilidade</div>
+                                            <div>
+                                                {product.countInStock > 0 ? (
+                                                    <span className="success">Em Estoque</span>
+                                                ) : (
+                                                    <span className="danger">Indisponível</span>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <button className="primary block">Adicionar ao Carrinho</button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
+
     );
 }
