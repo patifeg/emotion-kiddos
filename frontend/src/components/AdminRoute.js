600a42269f6824c613b8a01bcf9de751c85b85b0
Michel <patifeg@gmail.com>
2022-06-04 21:36:52 -0300

    Correção no nome para AdminRoute

--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { Outlet } from '../../node_modules/react-router-dom/index';
 
-export default function PrivateRoute({ component: Component, ...rest }) {
+export default function AdminRoute({ component: Component, ...rest }) {
     const userSignin = useSelector((state) => state.userSignin);
     const { userInfo } = userSignin;
     return userInfo && userInfo.isAdmin ? <Outlet /> : <Navigate to="/signin" />;
